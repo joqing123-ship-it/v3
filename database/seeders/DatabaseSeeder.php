f07d252e2eb972d201af84920eb753a7b2c4b54b
john90921 <sorvictor90@gmail.com>
2026-01-17 22:32:07 +0800

    lastes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,33 +15,33 @@ class DatabaseSeeder extends Seeder
     {
        $user = User::create([
             'email'=>'123@gmail.com',
-            'password'=>bcrypt('password'),
+            'password'=>bcrypt('123'),
             'role'=>'admin',
         ]);
         $user->profile()->create([
             'name'=>'Admin User',
         ]);
         // User::factory(10)->create();
-        User::factory()
-            ->workerRole()
-            ->hasProfile()
-            ->hasPosts(8)
-            ->hasLikes(5)
-            ->hasComments(4)
-            ->hasWorker()
-            ->hasReplies(5)
-            ->hasReports(2)
-            ->count(10)
-            ->create();
-        User::factory()
-            ->managerRole()
-            ->hasProfile()
-            ->hasReports(2)
-            ->hasPosts(8)
-            ->hasComments(4)
-            ->hasLikes(5)
-            ->hasReplies(5)
-            ->count(10)
-            ->create();
+        // User::factory()
+        //     ->workerRole()
+        //     ->hasProfile()
+        //     ->hasPosts(8)
+        //     ->hasLikes(5)
+        //     ->hasComments(4)
+        //     ->hasWorker()
+        //     ->hasReplies(5)
+        //     ->hasReports(2)
+        //     ->count(10)
+        //     ->create();
+        // User::factory()
+        //     ->managerRole()
+        //     ->hasProfile()
+        //     ->hasReports(2)
+        //     ->hasPosts(8)
+        //     ->hasComments(4)
+        //     ->hasLikes(5)
+        //     ->hasReplies(5)
+        //     ->count(10)
+        //     ->create();
     }
 }
